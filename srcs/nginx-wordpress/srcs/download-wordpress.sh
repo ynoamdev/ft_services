d09be3ca9d9b41f283c7ea33dd5eac411e8e9599
Youssef Noam <ynoam@e1r10p4.1337.ma>
2020-12-22 14:53:28 +0100

    aa

--- srcs/nginx-wordpress/srcs/download-wordpress.sh
+++ srcs/nginx-wordpress/srcs/download-wordpress.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# wget http://wordpress.org/latest.tar.gz
-# tar -xvf latest.tar.gz
-# mkdir -p /var/www/html/
-# mv wordpress /var/www/html/
+ wget http://wordpress.org/latest.tar.gz
+ tar -xvf latest.tar.gz
+ mkdir -p /var/www/html/
+ mv wordpress /var/www/html/
 # mv wp-config.php /var/www/html/wordpress
 # chown -R www-data:www-data /var/www/html/wordpress
-# chmod 755 -R /var/www/html/wordpress
+ chmod 755 -R /var/www/html/wordpress
--- srcs/nginx-wordpress/tst.sh
+++
@@ -1,6 +0,0 @@
-my_var="nixCraft"
-then
-      echo "\$my_var is NULL"
-else
-      echo "\$my_var is NOT NULL"
-fi
